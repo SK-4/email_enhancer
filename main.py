bdc0d8f1cc94b053c4c47fdf7d02c7ef8944992f
SK-4 <95765659+SK-4@users.noreply.github.com>
2023-04-07 21:49:50 +0530

    minor bug fixes ...

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ st.set_page_config(page_title="Globalize Email", page_icon=":robot:")
 st.header("Globalize Text")
 st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
             will help you improve your email skills by converting your emails into a more professional format. This tool \
-            is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) and made by \
-            [@GregKamradt](https://twitter.com/GregKamradt). \n\n View Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
+            is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com)
+            
 st.markdown("## Enter Your Email To Convert")
 
 def get_api_key():
